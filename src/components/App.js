54ea56c3e9ce08033e557b9e248aab7a178cf164
kaizengami <olexander.sj@gmail.com>
2019-04-22 16:14:10 +0300

    add fetch data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,91 @@
-import { Planets } from './Planets';
+import { Planets, PlanetModel, ResidentModel, SpeciesModel } from './Planets';
 import { getPlanets } from './utils/api';
 
 class App {
   constructor(host) {
+    this.state = {
+      data: null
+    };
     this.host = host;
     this.planets = new Planets();
   }
-  render() {
-    console.log(getPlanets());
+
+  async getApiData() {
+    const dataRaw = await getPlanets();
+    return dataRaw;
+  }
+
+  createPlanets(data) {
+    return data.map(planet => {
+      return new PlanetModel(planet);
+    });
+  }
+
+  createResident(data) {
+    return new ResidentModel(data);
+  }
+
+  createSpecies(data) {
+    return new SpeciesModel(data);
+  }
+
+  async getSwapiData(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      let data = await response.json();
+      return data;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async createAppData() {
+    await this.getApiData()
+      .then(data => {
+        const planets = data.results;
+        let newData = this.createPlanets(planets);
+        console.log(data);
+        return Promise.all(
+          newData.map(async (planet, i) => {
+            if (planet.residentsUrl.length) {
+              await Promise.all(
+                planet.residentsUrl.map(async residentUrl => {
+                  let residentDataRaw = await this.getSwapiData(residentUrl);
+                  let residentData = this.createResident(residentDataRaw);
+                  newData[i].residentsData.push(residentData);
+                })
+              );
+            }
+            return planet;
+          })
+        );
+      })
+      .then(data => {
+        data.map((planet, i) => {
+          let residents = planet.residentsData;
+          if (residents.length) {
+            residents.forEach(async (resident, n) => {
+              if (resident.speciesUrl.length) {
+                let speciesDataRaw = await this.getSwapiData(
+                  resident.speciesUrl
+                );
+                let speciesData = this.createSpecies(speciesDataRaw);
+                data[i].residentsData[n].speciesData.push(speciesData);
+              }
+            });
+          }
+        });
+        console.log(data);
+        return data;
+      });
+  }
+
+  async render() {
+    this.state.data = await this.createAppData();
+    console.log(this.state.data);
     this.host.innerHTML = this.planets.render();
   }
 }
--- src/components/Planets/index.js
+++ src/components/Planets/index.js
@@ -1,1 +1,2 @@
 export { default as Planets } from './Planets';
+export { PlanetModel, ResidentModel, SpeciesModel } from './Model';
